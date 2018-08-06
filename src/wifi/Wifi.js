4f16ffa5c2caef47c05b04f027c1a3fcdb011bc2
Reuben <reuben.eggar@me.com>
2018-08-06 08:03:37 +0100

    ✨ Cache Scan Results

--- src/wifi/Wifi.js
+++ src/wifi/Wifi.js
@@ -4,9 +4,14 @@ export default class Wifi {
   constructor(iface){
     this.iface = iface
     this.wireless = new Wireless(this.iface)
+
+    this.scanCache = {}
   }
 
   async scanForSSIDs(){
+    const cacheTime = 10 * 1000
+    if(this.scanCache.staleAt && this.scanCache.staleAt > Date.now()) return this.scanCache.scan
+
     const networksOnAir = await this.wireless.scan()
 
     const groupedBySSID = networksOnAir.reduce((SSIDs, {ssid: SSID, ...other}) => ({
@@ -14,12 +19,19 @@ export default class Wifi {
       [SSID]: [...(SSIDs[SSID] || []), {...other}]
     }), {})
 
-    return Object.entries(groupedBySSID)
+    const scan = Object.entries(groupedBySSID)
       .reduce((reformatted, [SSID, networkArray]) => [
         ...reformatted, {
           SSID,
           networks: networkArray.sort(({signal: a}, {signal: b}) => b - a)
         }], [])
       .sort(({networks: [{signal: a}]}, {networks: [{signal: b}]}) => b - a)
+
+    this.scanCache = {
+      scan,
+      staleAt: Date.now() + cacheTime
+    }
+
+    return scan
   }
 }
