e328cbe01c8cb2c68a62a051a0b145bf4f985859
Reuben <reuben.eggar@me.com>
2018-08-11 22:19:58 +0100

    ✨ Debug

--- src/front/store/feeds/sse.js
+++ src/front/store/feeds/sse.js
@@ -3,8 +3,8 @@ export default ({dispatch}) => {
 
   source.onmessage = event => {
     const {event: eventType, data} = JSON.parse(event.data)
+    console.log({eventType, data})
 
     if(eventType === 'status') return dispatch({type: 'STATUS_UPDATE', payload: data})
-    console.warn('Unhandled Event', {eventType, data})
   }
 }
