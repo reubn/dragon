6d18f470d2f05def1bffdb68ec818ea436bfe5e4
Reuben <reuben.eggar@me.com>
2018-08-12 19:11:06 +0100

    🐛

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const httpPort = 80
 export const wifiIface = 'wlan1'
 export const accessPointIface = 'wlan0'
-export const monitorUpdateFrequency = 2 * 1000
+export const monitorUpdateFrequency = 1.5 * 1000
+export const connectStatusUpdateDelay = 6 * 1000
 export const scanCacheTime = 10 * 1000
