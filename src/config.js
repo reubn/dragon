b634b33c28c1704c7b7e72d20554849f60e2554c
Reuben <reuben.eggar@me.com>
2018-08-12 13:11:40 +0100

    ✨ Better Hz

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const httpPort = 80
 export const wifiIface = 'wlan1'
 export const accessPointIface = 'wlan0'
-export const statusUpdateFrequency = 0.25 * 1000
+export const monitorUpdateFrequency = 2 * 1000
 export const scanCacheTime = 10 * 1000
