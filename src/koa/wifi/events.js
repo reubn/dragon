2a4ae50dfbd256e0445431a5b90f5a1116a658de
Reuben <reuben.eggar@me.com>
2018-08-12 13:10:37 +0100

    ✨

--- src/koa/wifi/events.js
+++ src/koa/wifi/events.js
@@ -3,10 +3,7 @@ import wifi from '../../wifi'
 
 export default async ctx => {
   const listener = (event, data) => ctx.sse.send(JSON.stringify({event: symbolDesc(event), data}))
-
   wifi.on(wifi.events.all, listener)
-
-  wifi.status()
 
   ctx.req.socket.on('close', () => wifi.removeListener(wifi.events.all, listener))
 }
