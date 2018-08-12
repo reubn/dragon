3c2153d143f420c6a9a94ef3408926b70b35b1cd
Reuben <reuben.eggar@me.com>
2018-08-12 13:10:26 +0100

    ✨ Request Status on Load

--- src/front/index.js
+++ src/front/index.js
@@ -4,8 +4,12 @@ import {render} from 'react-dom'
 import store from './store'
 import {linkHistoryToStore} from './routing'
 
+import getStatus from './store/actions/getStatus'
+
 import Root from './components/Root'
 
 linkHistoryToStore(store)
+
+getStatus(store.dispatch)
 
 render(<Root store={store} />, document.getElementById('app'))
