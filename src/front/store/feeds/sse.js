5fbdf8836da288ea48c4ea1359da47804d83e73c
Reuben <reuben.eggar@me.com>
2018-08-14 12:41:18 +0100

    🐛 iOS Drops SSE Connections for Unknown Reason

--- src/front/store/feeds/sse.js
+++ src/front/store/feeds/sse.js
@@ -1,11 +1,21 @@
 export default ({dispatch}) => {
-  const source = new EventSource('/wifi/events')
+  let source, retryTimeout;
 
-  source.onmessage = event => {
-    const {event: eventType, data} = JSON.parse(event.data)
-    console.log({eventType, data})
+  const initiate = () => {
+    if(source) source.close()
+    source = new EventSource('/wifi/events')
 
-    if(eventType === 'status') return dispatch({type: 'STATUS_UPDATE', payload: data})
-    if(eventType === 'scan') return dispatch({type: 'SCAN_RESULTS', payload: data})
+    source.onmessage = event => {
+      if(retryTimeout) clearTimeout(retryTimeout)
+      if(/iPad|iPhone|iPod/.test(navigator.userAgent)) retryTimeout = setTimeout(initiate, 7 * 1000)
+
+      const {event: eventType, data} = JSON.parse(event.data)
+      console.log({eventType, data})
+
+      if(eventType === 'status') return dispatch({type: 'STATUS_UPDATE', payload: data})
+      if(eventType === 'scan') return dispatch({type: 'SCAN_RESULTS', payload: data})
+    }
   }
+
+  initiate()
 }
